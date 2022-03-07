28514a6add10af1fc22eaabc905d01da7118d55b
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-07 08:33:42 +0700

    Các tờ tiền được lặp lại
    Tại ngân hàng có các mệnh giá bằng 1, 2, 5, 10, 20, 50, 100, 200, 500, 1000. Tổng số tiền cần đổi có giá trị bằng N.  Hãy xác định xem có ít nhất bao nhiêu tờ tiền sau khi đổi tiền?
    
    Input:
    
    Dòng đầu tiên là số lượng bộ test T (T ≤ 50).  Mỗi test gồm 1 số nguyên N ( 1 ≤ N ≤ 100 000).
    
    Output:  Với mỗi test, in ra đáp án trên một dòng.
    
    Ví dụ:
    
    Input:
    
    Output
    
    2
    
    70
    
    121
    
     
    
    2
    
    3

--- Giải thuật tham lam .cpp
+++ Giải thuật tham lam .cpp
@@ -1,1 +1,18 @@
 
+﻿#include<iostream>
+using namespace std;
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		int a[11] = {1,2,5,10,20,50,100,200,500,1000};
+		int money; cin >> money;
+		int count = 0;
+		for (int i = 9; i >=0; i--) {
+			count += money / a[i];
+			money = money % a[i];
+		}
+		cout << count << endl;
+	}
+	system("pause");
+	return  0;
+}
