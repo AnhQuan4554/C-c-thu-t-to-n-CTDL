49b466a1f76b8afe4e4aad7c766b51fb511a4ff6
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-11 10:09:33 +0700

    Create CHia để trị ( Lũy Thừa).cpp
    Cho số nguyên dương N và K. Hãy tính NK modulo 109+7.
    
    Input:
    
    Dòng đầu tiên là số lượng bộ test T (T ≤ 20).
    
    Mỗi test gồm 1 số nguyên N và K (1 ≤ N ≤ 1000, 1 ≤ K ≤ 109).
    
    Output: 
    
    Với mỗi test, in ra đáp án trên một dòng.
    
    Ví dụ:
    
    Input:
    
    Output
    
    2
    
    2 3
    
    4 2
    
    8
    
    16

---
+++ CHia để trị ( Lũy Thừa).cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+using namespace std;
+const long long modu = 1e9+7;
+long long Luy_thua(long long a, long long b) {
+	long long result = 1;
+	while (b) {
+		if (b % 2 == 1) {
+			result *= a;
+			result %= modu;
+		}
+		b /= 2;
+
+		a *= a;
+		a %= modu;
+	}return  result;
+}
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		int a, b; cin >> a >> b;
+		cout << Luy_thua(a, b) << endl;
+	}
+	system("pause");
+	return 0;
+
+}
