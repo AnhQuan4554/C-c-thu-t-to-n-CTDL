fa0e02f3bcf2d8cfe8a54282ef597cd27bd2abde
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-03-18 10:08:03 +0700

    Create Quay Lui+ vecto nâng cao.cpp
    Kỳ thi ICPC có K đội của PTIT tham gia và đội tuyển đang rất đau đầu không biết chọn các cái tên như thế nào cho các đội. Yêu cầu phải đảm bảo tên không có khoảng trống và không được trùng nhau. Sau khi thảo luận, có N cái tên được đề xuất (có thể bị trùng nhau). Với K<15 và 4 < N < 30.
    
    Hãy liệt kê tất cả danh sách các tổ hợp K cái tên khác nhau có thể được tạo ra theo thứ tự từ điển.
    
    Input
    
    Dòng đầu ghi 2 số N và K.
    
    Tiếp theo là 1 dòng ghi N cái tên, mỗi cái tên có độ dài không quá 15 và cách nhau một khoảng trống. Tất cả đều là ký tự in hoa.
    
    Output
    
    Ghi ra tất cả các tổ hợp tên có thể được lựa chọn theo thứ tự từ điển.
    
    Tức là các tên trong mỗi tổ hợp liệt kê theo thứ tự từ điển và các tổ hợp cũng được liệt kê theo thứ tự từ điển.
    
    Ví dụ
    
    Input
    
    Output
    
    6 2
    
    DONG TAY NAM BAC TAY BAC
    
    BAC DONG
    
    BAC NAM
    
    BAC TAY
    
    DONG NAM
    
    DONG TAY
    
    NAM TAY

---
+++ Quay Lui+ vecto nâng cao.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+#include<algorithm>
+#include<set>
+#include<vector>
+void input(set<string>& b, vector<pair<int, string>>&a, int n) {
+	for (int i = 1; i <= n; i++) {
+		string x; cin >> x;
+		b.insert(x);
+	}
+	int index = 1;
+	for (auto i : b) {
+	a[index].first = index;
+	a[index].second = i;
+	index++;
+	}
+}
+void khoitao(int * c, int k) {
+	for (int i = 1; i <= k; i++) {
+		c[i] = i;
+	}
+}
+void Try(int i, int* c,vector<pair<int,string>>&b, int n,int k) {
+	for (int j = c[i-1]+1; j <=n-k+i; j++) {
+		c[i] = j;
+		if (i == k) {
+			for (int x = 1; x <= k; x++) {
+				//cout << c[x] << " ";
+				cout << b[c[x]].second << " ";
+			}cout << endl;
+		}
+		else Try(i + 1, c, b, n,k);
+	}
+}
+
+int main() {
+		int n,k; cin >> n>>k  ;
+		set<string>b;
+		vector<pair<int, string>>a(n);
+		input(b, a, n);
+		int* c = new int[n];
+		khoitao(c, k);
+		c[0] = 0;
+		/*for (int i = 1; i <=b.size(); i++) {
+			cout << a[i].first <<" "<< a[i].second << endl;
+		}*/
+		Try(1, c,a,b.size(),k);
+	
+	system("pause");
+	return 0;
+}
