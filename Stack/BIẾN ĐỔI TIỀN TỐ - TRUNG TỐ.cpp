23dffcf4f1fb107ebe6c45777f5864bb3415f7f2
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-04-22 10:14:45 +0700

    duyệt từ cuối
    Hãy viết chương trình chuyển đổi biểu thức biểu diễn dưới dạng tiền tố về dạng trung tố.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T;
    Những dòng tiếp theo mỗi dòng đưa vào một bộ test. Mỗi bộ test là một biểu thức tiền tố exp.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Ràng buộc:
    
    T, exp thỏa mãn ràng buộc: 1≤T≤100; 2≤length(exp)≤106.
    Ví dụ:
    
    Input
    
    Output
    
    2
    
    *+AB-CD
    
    *-A/BC-/AKL
    
    ((A+B)*(C-D))
    
    ((A-(B/C))*((A/K)-L)

---
+++ Stack/BIẾN ĐỔI TIỀN TỐ - TRUNG TỐ.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+int main() {
+	int t;
+	cin >> t;
+	
+	cin.ignore();
+	while (t--) {
+		stack<string>a;
+		string x;
+		cin >> x;
+		for (int i = x.length()-1; i >=0; i--) {
+			if (x[i] == '+' || x[i] == '-' || x[i] == '*' || x[i] == '/') {
+				string firt = a.top(); a.pop();
+				string second = a.top(); a.pop();
+				string trung_gian = '(' + firt +x[i]+ second + ')';
+				a.push(trung_gian);
+			}
+			else {
+				a.push(string(1, x[i]));
+			}
+		}cout << a.top() << endl;
+	}
+	system("pause");
+	return 0;
+}
