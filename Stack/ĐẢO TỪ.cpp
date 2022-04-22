dc487517e7f9fc17841b542566c6cda65aa74d06
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-04-22 10:49:16 +0700

    Create ĐẢO TỪ.cpp
    Cho xâu ký tự S. Nhiệm vụ của bạn là đảo ngược các từ trong S. Ví dụ S =  “I like this program very much”, ta nhận được kết quả là “much very program this like I”.
    
    Input:
    
    Dòng đầu tiên đưa vào số lượng bộ test T.
    Những dòng kế tiếp đưa vào T bộ test. Mỗi bộ test là một xâu ký tự S.
    T, S thỏa mãn ràng buộc: 1≤ T ≤100; 1≤ Length(S)≤103.
    Output:
    
    Đưa ra kết quả mỗi test theo từng dòng.
    Ví dụ:
    
    Input:
    
    Output:
    
    2
    I like this program very much
    
    much very program this like I
    
    much very program this like I
    I like this program very much

---
+++ Stack/ĐẢO TỪ.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<stack>
+#include<string>
+#include<sstream>
+using namespace std;
+int main() {
+	int t;
+	cin >> t;
+	
+	cin.ignore();
+	while (t--) {
+		stack<string>a;
+		string x;
+		getline(cin, x);
+		stringstream ss(x);
+		string trung_gian;
+		while (ss >> trung_gian) {
+			a.push(trung_gian);
+		}
+		while (!a.empty()) {
+			cout << a.top() << " ";
+			a.pop();
+		}cout << endl;
+	}
+	system("pause");
+	return 0;
+}
