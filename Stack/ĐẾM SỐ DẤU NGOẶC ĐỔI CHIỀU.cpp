a01a48a8905c3a9bf59ee7bd4ffb407ff298e847
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-04-22 09:50:23 +0700

    số '("còn lại %2 + stack.size()/2
    Cho một xâu chỉ gồm các kí tự ‘(‘, ‘) và có độ dài chẵn. Hãy đếm số lượng dấu ngoặc cần phải đổi chiều ít nhất, sao cho xâu mới thu được là một dãy ngoặc đúng.
    
    Input:
    
    Dòng đầu tiên là số lượng bộ test T (T ≤ 20).
    
    Mỗi test gồm 1 xâu S có độ dài không vượt quá 100 000, chỉ gồm dấu ( và ).
    
    Output: 
    
    Với mỗi test, in ra đáp án tìm được trên một dòng.
    
    Ví dụ:
    
    Input:
    
    Output
    
    4
    
    ))((
    
    ((((
    
    (((())
    
    )(())(((
    
    2
    
    2
    
    1
    
    3

---
+++ Stack/ĐẾM SỐ DẤU NGOẶC ĐỔI CHIỀU.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+int main() {
+	int t;
+	cin >> t;
+	
+	cin.ignore();
+	while (t--) {
+		stack<int>a;
+		string x;
+		getline(cin, x);
+		for (int i = 0; i < x.length(); i++) {
+			if (x[i] == ')' && !a.empty()) {
+				if (a.top() == '(')a.pop();
+				else a.push(x[i]);
+			}
+			else {
+				a.push(x[i]);
+			}
+		}
+		int n = 0;
+		int k = a.size();
+		while (a.size() && a.top() == '(') {
+			n++;
+			a.pop();
+		}
+		cout << k / 2 +( n % 2) << endl;
+
+
+	}
+	system("pause");
+	return 0;
+}
