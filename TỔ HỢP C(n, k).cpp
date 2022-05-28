2e9bcd9621bfad229e8278a8886ef10ae0e200d9
AnhQuan4554 <90078555+AnhQuan4554@users.noreply.github.com>
2022-05-28 10:16:25 +0700

    Create TỔ HỢP C(n, k).cpp
    Cho 2 số nguyên n, k. Bạn hãy tính C(n, k) modulo 109+7.
    
    Input:
    
    Dòng đầu tiên là số lượng bộ test T (T ≤ 20).
    Mỗi test gồm 2 số nguyên n, k (1 ≤ k ≤ n ≤ 1000).
    Output: 
    
    Với mỗi test, in ra đáp án trên một dòng.
    Ví dụ:
    
    Input
    
    Output
    
    2
    
    5 2
    
    10 3
    
     
    
    10
    
    120

---
+++ TỔ HỢP C(n, k).cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<set>
+using namespace std;
+#include<deque>
+#include<algorithm>
+
+typedef long long ll;
+ll modu = 1e9 + 7;
+ll a[1005][1005];
+void To_hop() {
+	for (int i = 0; i <= 1001; i++) {
+		
+		for (int j = 0; j <= i; j++) {
+			if (i == j || j == 0)a[i][j] = 1;
+			else a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
+			a[i][j] %= modu;
+		}
+	}
+}
+int main() {
+	To_hop();
+	int t; cin >> t;
+	while (t--) {
+		int n, k; cin >> n >> k;
+		cout << a[n][k] << endl;
+	}
+	system("pause");
+	return 0;
+}
